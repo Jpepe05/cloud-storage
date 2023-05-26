1beee34abf3779f475ff87c55b95fa40617d63be
Joao Ribeiro <joaopedrocastro05@gmail.com>
2023-05-26 00:06:56 -0300

    remove unused

--- src/main/java/com/ti5/cloudstorage/CloudStorageApplication.java
+++ src/main/java/com/ti5/cloudstorage/CloudStorageApplication.java
@@ -2,10 +2,8 @@ package com.ti5.cloudstorage;
 
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class CloudStorageApplication {
 
   public static void main(String[] args) {
